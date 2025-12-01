5669bf1991ff75628908a1a7b5203612ad222436
Theo Cohen <tcohen@f4r7s16.clusters.42paris.fr>
2025-12-01 11:04:13 +0100

    part tester done and validated + leaks free

--- test/part_test.sh
+++ test/part_test.sh
@@ -4,7 +4,7 @@ HOST="localhost"
 PORT="6667"
 
 {
-    sleep 1
+    sleep 3
     echo "PASS theo"
     sleep 0.2
     echo "NICK tester"
@@ -19,7 +19,29 @@ PORT="6667"
     sleep 0.2
     echo "PART #chips"
     sleep 0.2
+    echo "PART #test"
+    sleep 0.2
     echo "JOIN #chips"
     sleep 0.2
     echo "PART #chips ain't gott no reason okay ?! I just want to leave"
+} | nc -C $HOST $PORT &
+
+########################################
+# CLIENT 2 : watcher (nouveau terminal)
+########################################
+gnome-terminal -- bash -c "
+{
+    sleep 1
+    echo \"PASS theo\"
+    sleep 0.2
+    echo \"NICK watcher\"
+    sleep 0.2
+    echo \"USER u3 0 * :WatchingYourAss\"
+    sleep 0.3
+    echo \"JOIN #chips\"
+    sleep 0.3
+    echo \"JOIN #test\"
+
 } | nc -C $HOST $PORT
+exec bash
+"
